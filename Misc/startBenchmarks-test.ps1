098385754716f6577a9cb414b4f97b4d6023c6a1
Kenneth Stebbins <contact@kennethstebbins.com>
2020-06-03 11:13:33 -0600

    Fixed a typo and removed exit confirmation

--- Misc/startBenchmarks-test.ps1
+++ Misc/startBenchmarks-test.ps1
@@ -1,15 +1,12 @@
 Param(
-    [String]$DBDirecotryPath,
+    [String]$DBDirectoryPath,
     [String]$TestDefinitionsPath,
     [String]$StartBenchmarkModulePath
 )
 
 Import-Module "$StartBenchmarkModulePath"
 
-Start-Benchmark -DBDirectoryPath "$DBDirecotryPath" -TestDefinitionsPath "$TestDefinitionsPath"
-
-Write-Host -ForegroundColor White "Press ENTER to exit..."
-Read-Host | Out-Null
+Start-Benchmark -DBDirectoryPath "$DBDirectoryPath" -TestDefinitionsPath "$TestDefinitionsPath"
 
 Remove-Module Start-Benchmark
 Remove-Module Benchmark* -ErrorAction SilentlyContinue
