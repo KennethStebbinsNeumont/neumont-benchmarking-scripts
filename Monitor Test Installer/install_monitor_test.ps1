4bbc483d54ff12ca6c918ee42c97fb0feabf27c6
Kenneth Stebbins <contact@kennethstebbins.com>
2020-06-03 14:33:02 -0600

    Monitor test install script now self-elevates

--- Monitor Test Installer/install_monitor_test.ps1
+++ Monitor Test Installer/install_monitor_test.ps1
@@ -1,3 +1,13 @@
-New-Item -Path 'C:\Program Files (x86)\MonitorTest\' -ItemType Directory -ErrorAction Continue -InformationAction Ignore
+# https://blog.expta.com/2017/03/how-to-self-elevate-powershell-script.html
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+     $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+     Exit
+    }
+}
+
+New-Item -Path 'C:\Program Files (x86)\MonitorTest\' -ItemType Directory -ErrorAction Stop -InformationAction Ignore
 
 Copy-Item -Path "$PSScriptRoot\monitorTest.exe" -Destination 'C:\Program Files (x86)\MonitorTest\'
